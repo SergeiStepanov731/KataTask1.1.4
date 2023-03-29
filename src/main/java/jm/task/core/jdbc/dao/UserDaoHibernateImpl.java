0e86bc67d7079365ed31ac5b3bf0fb993e96ecfe
SergeiStepanov731 <iffymailone@gmail.com>
2023-03-29 18:38:49 +0300

    update
    update

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -9,6 +9,13 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
+    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS  Users (id SERIAL PRIMARY KEY, name VARCHAR(20),lastname VARCHAR(20),age INT)";
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS Users";
+    private static final String SAVE_USER = "INSERT INTO Users(name,lastname,age) VALUES (?,?,?)";
+    private static final String DELETE_BY_ID = "DELETE FROM Users WHERE id = ?";
+    private static final String GET_USERS = "SELECT * FROM Users";
+    private static final String CLEAN_TABLE = "TRUNCATE TABLE Users";
+
 
     @Override
     public void createUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,6 +5,11 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class Util {
     private static final String URL="jdbc:mysql://localhost:3306/KataTask01";
     private static final String USERNAME="bestuser";
@@ -28,4 +33,26 @@ public class Util {
 
     }
 
+    private static final String DB_DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String DIALECT = "org.hibernate.dialect.MySQL8Dialect";
+    private static final String SHOW_SQL = "true";
+    private static final String HBM2DLL = "create";
+    private static final String CURRENT_SESSION = "thread";
+
+
+    public static SessionFactory getSessionFactory() {
+
+        Configuration config = new Configuration();
+        config.setProperty("hibernate.connector.driver_class", DB_DRIVER_NAME);
+        config.setProperty("hibernate.connection.url", URL);
+        config.setProperty("hibernate.connection.username", USERNAME);
+        config.setProperty("hibernate.connection.password", PASSWORD);
+        config.setProperty("hibernate.dialect", DIALECT);
+        config.setProperty("hibernate.show_sql", SHOW_SQL);
+        config.setProperty("hibernate.hbm2ddl.auto", HBM2DLL);
+        config.setProperty("hibernate.current_session_context_class", CURRENT_SESSION);
+
+        return config.addAnnotatedClass(User.class).buildSessionFactory();
+    }
 }
+
